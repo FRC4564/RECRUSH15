a6ff6c8e6b8e721bc0aa173283d54a651ba4af86
FRC4564 <Chaos@Robot3.breweredu.org>
2015-01-20 21:08:35 -0500

    Built PID based Lift methods

--- RecRush15/src/org/usfirst/frc/team4564/robot/Lift.java
+++ RecRush15/src/org/usfirst/frc/team4564/robot/Lift.java
@@ -14,33 +14,40 @@ public class Lift {
     private Victor liftMotor = new Victor(Constants.PWM_LIFT_MOTOR);
     private Encoder encoder = new Encoder(Constants.DIO_LIFT_ENCODER_A, Constants.DIO_LIFT_ENCODER_B,
     									  false, EncodingType.k4X);
+    
+    //State Constants
+    private static final int LIFT_STOPPED = 0;
+	private static final int LIFT_INIT = 1;
+	private static final int LIFT_IDLE = 2;
+	private static final int LIFT_MOVING = 3;
+    //Lift variables
+    private int liftState = LIFT_STOPPED;
+    private double liftSpeed = 0;
+    private int liftTarget = 0;
+    //PID constants
+    private static final double Kp = 0.3;
+    private static final double MAX_SPEED = 1.0;
 	// encoder values
 	private static final int COUNTS_PER_INCH = 100;
-	private int targetPosition;  // Encoder count
+    private static final int TOLERANCE = 2;   //allowable tolerance between target and encoder
 	// motor constants
 	private static final boolean MOTOR_INVERT = false; // inverted means positive motor values move down
 	private static final double MOTOR_HOME_SPEED = -0.25;  //speed to move lift with finding home position
 	// limit switch constants
 	private static final boolean UPPER_LIMIT_PRESSED = true;
 	private static final boolean LOWER_LIMIT_PRESSED = true;
-	
-	
-	
-	
-	// Intialize lift
-	public void init() {
-		home();
-	}
-	
+
 	// Find home - which is at lowest position
-	public void home() {
+	public void updateInit() {
 		// Move to lower limit switch
-		while (lowerLimit.get() != LOWER_LIMIT_PRESSED) {
+		if (lowerLimit.get() != LOWER_LIMIT_PRESSED) {
 			moveLift(MOTOR_HOME_SPEED);
 			SmartDashboard.putNumber("encoder", encoder.get());
+		} else {
+			// Set encoder count to 0, which is considered 'home'
+			encoder.reset();
+			liftState = LIFT_IDLE;
 		}
-		// Set encoder count to 0, which is considered 'home'
-		encoder.reset();
 	}
 	
 	
@@ -51,7 +58,7 @@ public class Lift {
 		boolean directionUp = true;
 		if (motorSpeed < 0) {
 				directionUp = false;
-			}
+		}
 		// adjust direction of motorSpeed if it needs to be inverted
 		if (MOTOR_INVERT) {
 			motorSpeed = -motorSpeed;
@@ -61,20 +68,58 @@ public class Lift {
 		if (directionUp) {
 			if (upperLimit.get() == UPPER_LIMIT_PRESSED) {
 				liftMotor.set(0);
+				liftTarget = encoder.get();
+				liftSpeed = 0;
 			} else {
 				liftMotor.set(motorSpeed);
 			}
 		} else {
 			if (lowerLimit.get() == LOWER_LIMIT_PRESSED) {
 				liftMotor.set(0);
+				liftTarget = encoder.get();
+				liftSpeed = 0;
 			} else {
 				liftMotor.set(motorSpeed);
 			}
 		}
 	}
 	
-	public void update() {
-		SmartDashboard.putNumber("encoder", encoder.get());
+	private void PIDMove() {
+	    double error = 0;
+	    double P = 0;
+		// Calculate PID
+		error = liftTarget - encoder.get();
+		
+		P = error * Kp;
+		liftSpeed = P;
+		if (liftSpeed > MAX_SPEED) {
+			liftSpeed = MAX_SPEED;
+		} else if (liftSpeed < -MAX_SPEED) {
+			liftSpeed = -MAX_SPEED;
+		}
+		moveLift(liftSpeed);
 	}
-
+	
+	public void updateMove() {
+		if (Math.abs(encoder.get() - liftTarget) <= TOLERANCE) {
+			liftState = LIFT_IDLE;
+		} else {
+			PIDMove();
+		}
+	}
+	public void update() {
+		
+		if (liftState == LIFT_STOPPED) {
+			liftState = LIFT_INIT;
+		} else if (liftState == LIFT_INIT) {
+			updateInit();
+		} else if (liftState == LIFT_IDLE) {
+						
+		} else if (liftState == LIFT_MOVING) {
+			updateMove();
+		}
+		SmartDashboard.putNumber("encoder", encoder.get());
+			
+		
+	}
 }
